c3a3204a8b7b1db76cbc0afda261f7d7e4936374
Austin <austin.gray4292@gmail.com>
2019-06-24 05:34:59 -0700

    Populates newsfeed and ticket list from arrays

--- app/newsfeed/newsfeed.component.js
+++ app/newsfeed/newsfeed.component.js
@@ -3,5 +3,25 @@
 angular.
   module('newsfeed').
   component('newsfeed', {
-    templateUrl: 'newsfeed/newsfeed.template.html'
+    templateUrl: 'newsfeed/newsfeed.template.html',
+    controller: function NewsfeedController() {
+      this.newsfeed = [
+        {
+          details: 'The new Global Business Services portal is now live. This effort presents users with a unified web interface for employees to access information and resources related to HT, IT, and Procurement. The n',
+          headline: 'GBS Portal Now Live!',
+          location: 'General',
+          time: '7d ago'
+        }, {
+          details: 'The new Global Business Services portal is now live. This effort presents users with a unified web interface for employees to access information and resources related to HT, IT, and Procurement. The n',
+          headline: 'GBS Portal Now Live!',
+          location: 'General',
+          time: '15d ago'
+        }, {
+          details: 'The new Global Business Services portal is now live. This effort presents users with a unified web interface for employees to access information and resources related to HT, IT, and Procurement. The n',
+          headline: 'GBS Portal Now Live!',
+          location: 'General',
+          time: '24d ago'
+        }
+      ];
+    }
   });
--- app/tickets/tickets.component.js
+++ app/tickets/tickets.component.js
@@ -3,5 +3,22 @@
 angular.
   module('tickets').
   component('tickets', {
-    templateUrl: 'tickets/tickets.template.html'
+    templateUrl: 'tickets/tickets.template.html',
+    controller: function TicketsController() {
+      this.tickets = [
+        {
+          details: 'Engineering Laptop - HP ZBook 17 G3 Bundle',
+          id: 'REQ0078372',
+          time: '13h ago',
+        }, {
+          details: 'Engineering Laptop - HP ZBook 17 G3 Bundle',
+          id: 'REQ0078371',
+          time: '7d ago',
+        }, {
+          details: '3 requested items',
+          id: 'REQ0078333',
+          time: '17d ago',
+        }
+      ];
+    }
   });
